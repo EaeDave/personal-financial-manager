badf6b1535ef9fe46766266448e61b80cc2ed6b7
EaeDave <d4vd.sv@gmail.com>
2026-01-28 15:50:32 -0300

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,21 @@
+//  @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+  enable: true,
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  endOfLine: 'lf',
+  singleAttributePerLine: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  jsxSingleQuote: true,
+  arrowParens: 'always',
+  trailingComma: 'es5',
+  quoteProps: 'as-needed',
+}
+
+export default config
